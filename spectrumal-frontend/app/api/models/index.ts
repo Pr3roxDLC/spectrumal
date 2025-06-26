d1b6468fd1e18ad95f85fcc5969362aeaf05d083
Tim.Heller <tim.heller@cas.de>
2025-06-27 01:17:34 +0200

    Configure api client and create docker setup

---
+++ spectrumal-frontend/app/api/models/index.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CreateGameResponse';
+export * from './CreateLobbyRequest';
+export * from './CreateLobbyResponse';
+export * from './Dimension';
+export * from './JoinLobbyRequest';
+export * from './JoinLobbyResponse';
+export * from './Point';
+export * from './PointGuessRequest';
+export * from './RoundInfo';
+export * from './RoundInfoResponse';
+export * from './Score';
+export * from './ScoreResponse';
+export * from './User';
+export * from './WordGuess';
+export * from './WordGuessRequest';
+export * from './WordGuessResponse';
